d913a84a643a15eb2fb231e051ee55371dacbdae
Jonathan Krall <jonathan.krall@lexicon.se>
2025-05-27 11:58:28 +0200

    Added Person

---
+++ BasicOOPLTU/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOPLTU
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty; 
+    }
+
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+
+    internal class Admin: Employee
+    {
+        public string Department { get; set; } = string.Empty; 
+    }
+}
